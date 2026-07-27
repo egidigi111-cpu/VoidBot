2214c33c58fa4f2a8aebb9d4d0de56349ab98b57
voidbot <voidbot@localhost>
2026-07-27 04:11:58 +0200

    refactor: rename /log set to /highlogs

--- commands/log.js
+++ commands/log.js
@@ -3,24 +3,17 @@ const configManager = require('../utils/configManager');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('log')
-    .setDescription('Log system configuration')
-    .addSubcommand(sub =>
-      sub.setName('set')
-        .setDescription('Set the log channel')
-        .addChannelOption(opt =>
-          opt.setName('channel')
-            .setDescription('The log channel')
-            .setRequired(true)
-            .addChannelTypes(ChannelType.GuildText)))
+    .setName('highlogs')
+    .setDescription('Set the audit log channel')
+    .addChannelOption(opt =>
+      opt.setName('channel')
+        .setDescription('The log channel')
+        .setRequired(true)
+        .addChannelTypes(ChannelType.GuildText)))
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
   async execute(interaction) {
-    const sub = interaction.options.getSubcommand();
-
-    if (sub === 'set') {
-      const channel = interaction.options.getChannel('channel');
-      configManager.set('logChannelId', channel.id);
-      await interaction.reply({ content: `Log channel set to ${channel}.`, ephemeral: true });
-    }
+    const channel = interaction.options.getChannel('channel');
+    configManager.set('logChannelId', channel.id);
+    await interaction.reply({ content: `Log channel set to ${channel}.`, ephemeral: true });
   },
 };
